a66c9f1bc4cdc9adbeb21e0d02a7ae61413877a9
anhhao17 <hiimanhhao@gmail.com>
2022-03-18 21:06:26 +0700

    add

---
+++ app/src/main/java/com/first_java_app/k_login_signup/Mydialog.kt
@@ -0,0 +1,20 @@
+package com.first_java_app.k_login_signup
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class Mydialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder= AlertDialog.Builder(it)
+            builder.setView(requireActivity().layoutInflater.inflate(R.layout.layout_dialog,null))
+            builder.setPositiveButton("Summit", DialogInterface.OnClickListener({ dialog, id->
+
+            }))
+            builder.create()
+        }?:throw IllegalStateException("Activity is Null!!")
+    }
+}
